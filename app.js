87666536c6c39d96c36a273c2c2baa14ed85309a
camibazan28 <camibazan79@gmail.com>
2024-08-13 20:18:17 -0400

    examen

---
+++ app.js
@@ -0,0 +1,20 @@
+
+//crear un algoritmo, basado en la nota
+//imprima si el alumno aprueba la materia
+// aprueba 60
+//imprimir un mensaje que diga por favor que estudie mas
+
+
+let nota = 58;
+
+if(nota >= 60){
+    console.log('El alumno aprueba la clase');
+}  else if(nota< 1) {
+
+    console.log('Por Favor estudie mas');
+    }  else {
+        console.log('El alumno NO aprueba la clase');
+}
+
+
+console.log('Fin del programa');
